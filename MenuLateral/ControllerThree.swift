cd80a1b4feff6a443e69e9b3fe7590f93acf5e8b
Joel Backschat <joel@cafecomjava.com.br>
2016-02-01 20:04:19 -0200

    commit inicial

---
+++ MenuLateral/ControllerThree.swift
@@ -0,0 +1,18 @@
+//
+//  ControllerThree.swift
+//  MenuLateral
+//
+//  Created by Joel Backschat on 1/31/16.
+//  Copyright © 2016 Joel Backschat. All rights reserved.
+//
+
+import Foundation
+
+
+class ControllerTree : UIViewController{
+    @IBOutlet weak var btnOpen: UIBarButtonItem!
+    override func viewDidLoad() {
+        btnOpen.target = self.revealViewController()
+        btnOpen.action = Selector("revealToggle:")
+    }
+}
